947e881781da01568c45cc3b2525b46507b2a6d0
ztonege <nianhsuan@gmail.com>
2022-04-11 00:59:28 +0000

    clientsPool, register and remove clients

--- src/github.com/cmu440/p0partA/server_impl.go
+++ src/github.com/cmu440/p0partA/server_impl.go
@@ -5,6 +5,7 @@ package p0partA
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"os"
 
@@ -13,14 +14,34 @@ import (
 
 type keyValueServer struct {
 	// TODO: implement this!
-	store kvstore.KVStore
-	port  int
+	store               kvstore.KVStore
+	port                int
+	clientPool          map[string]*client
+	registrationChannel chan *client
+	removalChannel      chan string
+	dropped             int
+}
+
+type client struct {
+	id     string
+	reader *bufio.Reader
+	writer *bufio.Writer
+	conn   net.Conn
+	// FIXME: think of a better name
+	channel chan string
+}
+
+func (cli client) String() string {
+	return fmt.Sprintf("Client: %v", cli.id)
 }
 
 // New creates and returns (but does not start) a new KeyValueServer.
 func New(store kvstore.KVStore) KeyValueServer {
 	// TODO: implement this!
-	kvs := &keyValueServer{store: store}
+	clientPool := map[string]*client{}
+	registrationChannel := make(chan *client)
+	removalChannel := make(chan string)
+	kvs := &keyValueServer{store: store, clientPool: clientPool, registrationChannel: registrationChannel, removalChannel: removalChannel}
 	return kvs
 }
 
@@ -34,42 +55,14 @@ func (kvs *keyValueServer) Start(port int) error {
 		// FIXME: can't use os package.
 		os.Exit(1)
 	}
-
 	kvs.port = port
-	go func() {
-		for {
-			// Wait for a connection
-			conn, err := ln.Accept()
-			if err != nil {
-				// FIXME: remove print line.
-				fmt.Println("Error accepting: ", err.Error())
-				// FIXME: can't use os package
-				os.Exit(1)
-			}
-			go handleConnection(conn)
-		}
-	}()
+	// Client registration and removal handling
+	go poolAdminstration(kvs)
+
+	// Concurrently listen to multiple clients
+	go handleClients(ln, kvs)
 
 	return err
-}
-
-// Handles incoming requests.
-func handleConnection(conn net.Conn) {
-	strRemoteAddr := conn.RemoteAddr().String()
-	reader, writer := bufio.NewReader(conn), bufio.NewWriter(conn)
-	for {
-		message, readErr := reader.ReadString('\n')
-		if readErr != nil {
-			fmt.Println("Error reading:", readErr.Error())
-		}
-		// Send a response back to person contacting us.
-		_, writeErr := writer.WriteString("Message Received: " + message)
-		if writeErr != nil {
-			fmt.Println("Error writing:", writeErr.Error())
-		}
-		writer.Flush()
-		fmt.Printf("[%v] %v", strRemoteAddr, message)
-	}
 }
 
 func (kvs *keyValueServer) Close() {
@@ -78,12 +71,87 @@ func (kvs *keyValueServer) Close() {
 
 func (kvs *keyValueServer) CountActive() int {
 	// TODO: implement this!
-	return -1
+	return len(kvs.clientPool)
 }
 
 func (kvs *keyValueServer) CountDropped() int {
 	// TODO: implement this!
-	return -1
+	return kvs.dropped
 }
 
-// TODO: add additional methods/functions below!
+// Client registration and removal handling
+func poolAdminstration(kvs *keyValueServer) {
+	for {
+		select {
+		// Handle client registrations
+		case cli := <-kvs.registrationChannel:
+			kvs.clientPool[cli.id] = cli
+		// Handle client removals
+		case id := <-kvs.removalChannel:
+			_, ok := kvs.clientPool[id]
+			if !ok {
+				fmt.Printf("%v does not exist in clientPool!", id)
+			}
+			delete(kvs.clientPool, id)
+			kvs.dropped++
+		}
+	}
+}
+
+func handleClients(ln net.Listener, kvs *keyValueServer) {
+	for {
+		// Wait for a connection
+		conn, err := ln.Accept()
+		if err != nil {
+			// FIXME: remove print line.
+			fmt.Println("Error accepting: ", err.Error())
+			// FIXME: can't use os package
+			os.Exit(1)
+		}
+		go handleConnection(conn, kvs)
+	}
+}
+
+// Handles incoming requests.
+func handleConnection(conn net.Conn, kvs *keyValueServer) {
+	// Register client: create client + add to client pool
+	cli := registerClient(conn, kvs)
+	fmt.Printf("[%v] connected!\n", cli.id)
+	// Listen to client
+	for {
+		// Listen to client
+		message, err := cli.reader.ReadString('\n')
+		switch err {
+		// Handle Get, Put, Update, Delete
+		case nil:
+			// Send a response back to person contacting us.
+			_, writeErr := cli.writer.WriteString("Message Received: " + message)
+			if writeErr != nil {
+				fmt.Println("Error writing:", writeErr.Error())
+			}
+			cli.writer.Flush()
+			fmt.Printf("[%v] %v", cli.id, message)
+		// Check if client closed or terminated connection
+		case io.EOF:
+			removeClient(cli.id, kvs)
+			fmt.Printf("[%v] disconnected\n", cli.id)
+			return
+		default:
+			fmt.Printf("[%v] error\n", cli.id)
+		}
+	}
+}
+
+func registerClient(conn net.Conn, kvs *keyValueServer) *client {
+	id := conn.RemoteAddr().String()
+	reader, writer := bufio.NewReader(conn), bufio.NewWriter(conn)
+	channel := make(chan string)
+	cli := &client{id: id, reader: reader, writer: writer, conn: conn, channel: channel}
+	// FIXME: only sends to registration channel now, does not wait for confirmation
+	kvs.registrationChannel <- cli
+	return cli
+}
+
+func removeClient(id string, kvs *keyValueServer) {
+	kvs.removalChannel <- id
+}
